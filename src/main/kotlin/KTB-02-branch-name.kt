5d69f4ce932ff6cd16345142e3c4f7a1122b0c56
Alexsandra97 <thefirstone010101@gmail.com>
2024-11-04 20:25:07 +1000

    KTB-02-branch-name

--- src/main/kotlin/KTB-02-branch-name.kt
+++ src/main/kotlin/KTB-02-branch-name.kt
@@ -3,6 +3,18 @@ import java.io.File
 fun main() {
 
     val wordsFile: File = File("word.txt")
-    (wordsFile.readLines()).forEach { println(it) }
+
+    val lines: List<String> = wordsFile.readLines()
+    for (line in lines) {
+        val line = line.split("|")
+        val word = Word(original = line[0], translate = line[1])
+        println(word)
+
+    }
 
 }
+
+data class Word(
+    val original: String,
+    val translate: String,
+)
