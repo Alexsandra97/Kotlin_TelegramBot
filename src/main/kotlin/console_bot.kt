972d6d166f2d8b1ac5ba2c805f65a70148f91e47
Alexsandra97 <thefirstone010101@gmail.com>
2024-11-07 20:52:40 +1000

    add code

--- src/main/kotlin/console_bot.kt
+++ src/main/kotlin/console_bot.kt
@@ -3,10 +3,12 @@ import java.io.File
 fun main() {
 
     val wordsFile: File = File("word.txt")
+    val dictionary = mutableListOf<Word>()
 
     val lines: List<String> = wordsFile.readLines()
     for (line in lines) {
         val line = line.split("|")
+        val word = Word(original = line[0], translate = line[1], correctAnswersCount = (line[2]?.toIntOrNull() ?: 0))
         dictionary.add(word)
 
     }
@@ -14,6 +16,7 @@ fun main() {
     println(dictionary)
 }
 
+data class Word(
     val original: String,
     val translate: String,
     var correctAnswersCount: Int,
